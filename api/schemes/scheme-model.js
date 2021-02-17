0e010a575350feeac9d8e0650fbbf6407c5c1f5b
Plofland <ploflan@gmail.com>
2021-02-17 17:39:04 -0500

    Models: find, findById, findSteps and add are functional

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,2 +1,50 @@
 // scheme-model
-const db = require('')
+const { where } = require('../../data/db-config');
+const db = require('../../data/db-config');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+  remove
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes').where('id', id).first();
+}
+
+function findSteps(id) {
+  return db('schemes as sc')
+    .join('steps as st', 'st.scheme_id', 'sc.id')
+    .where('sc.id', id)
+    .select('sc.id', 'sc.scheme_name', 'st.step_number', 'st.instructions')
+}
+
+function add(schemeData) {
+  return db('schemes')
+    .insert(schemeData)
+    .then(([id]) => {
+      return db('schemes').where('id', id).first();
+    });
+}
+
+function addStep(stepData, id) {
+  const stepId = id
+  return db('schemes')
+    .where('id', id)
+    .update(stepData)
+    .then(() => {
+      return db('schemes').where('id', stepId).first()
+    })
+}
+
+function update() {}
+
+function remove() {}
--- queries.sql
+++ queries.sql
@@ -1,9 +1,22 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select p.productname, c.categoryname
+from product as p
+join category as c
+on p.categoryId = c.id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+--select od.orderid, o.shipname, o.orderdate
+--from orderdetail as od
+--join "order" as o
+--on od.orderid = o.id
+--where o.orderdate before "2012-08-09"
+--⬆GET THIS WORKING⬆
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
