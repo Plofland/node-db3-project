0ffd779aae734ae061e06e7aeeb5fa73adab5ad6
Plofland <ploflan@gmail.com>
2021-02-17 22:59:39 -0500

    Finished update scheme-model stretch goal and SQL questions

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -24,7 +24,12 @@ function findSteps(id) {
   return db('schemes as sc')
     .join('steps as st', 'st.scheme_id', 'sc.id')
     .where('sc.id', id)
-    .select('sc.id', 'sc.scheme_name', 'st.step_number', 'st.instructions')
+    .select(
+      'sc.id',
+      'sc.scheme_name',
+      'st.step_number',
+      'st.instructions'
+    );
 }
 
 function add(schemeData) {
@@ -36,23 +41,21 @@ function add(schemeData) {
 }
 
 function addStep(stepData, id) {
-  const stepId = id
-  return db('schemes')
-    .where('id', id)
-    .update(stepData)
-    .then(() => {
-      return db('schemes').where('id', stepId).first()
-    })
+  const stepId = id;
+  return db('steps').insert(stepData).where('id', id)
+  .then(() => {
+    return db('schemes').where('id', stepId).first();
+  });
 }
 
 function update(changes, id) {
-  const schemeId = id
+  const schemeId = id;
   return db('schemes')
     .where('id', id)
     .update(changes)
     .then(() => {
-      return db('schemes').where('id', schemeId).first()
-    })
+      return db('schemes').where('id', schemeId).first();
+    });
 }
 
 function remove(id) {
@@ -60,6 +63,6 @@ function remove(id) {
     .where('id', id)
     .del()
     .then(() => {
-      return db('schemes')
-    })
+      return db('schemes');
+    });
 }
--- queries.sql
+++ queries.sql
@@ -7,16 +7,28 @@ join category as c
 on p.categoryId = c.id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
---select od.orderid, o.shipname, o.orderdate
---from orderdetail as od
---join "order" as o
---on od.orderid = o.id
---where o.orderdate before "2012-08-09"
---⬆GET THIS WORKING⬆
+select o.customerid, s.companyname, o.orderdate
+from "order" as o
+join shipper as s
+on o.shipvia = s.id
+where o.orderdate < "2012-08-09"
 
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
+select p.productName, od.quantity, od.orderid
+from orderdetail as od
+join product as p
+on p.id = od.productid 
+where od.orderid = 10251
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 
+select 
+    o.id,
+    c.companyname,
+    e.lastname
+from "order" as o
+join customer as c
+on c.id = o.customerid
+join employee as e
+on e.id = o.employeeid
